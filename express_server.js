4d16a7a742d5f2dda26b35d462e7ae5339b12508
Neriman Dikmen <sanerneriman@gmail.com>
2023-06-23 21:29:07 -0400

    Add get.app for urls/new & setting up the Post route

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const urlDatabase = {
   '9sm5xK': "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -18,9 +20,12 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  console.log(req);
   res.render('urls_show', templateVars);
   });
 
