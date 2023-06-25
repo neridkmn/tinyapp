2dac7d062f7281f5cb068f63c19d551aa0c14adc
Neriman Dikmen <sanerneriman@gmail.com>
2023-06-25 13:08:30 -0400

    Add /urls post endpoint

--- express_server.js
+++ express_server.js
@@ -1,3 +1,15 @@
+function generateRandomString() {
+  let shortUrlArray = [];
+  let alphanumericCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  for (i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * alphanumericCharacters.length);
+    shortUrlArray.push(alphanumericCharacters[randomIndex]);
+  }
+
+  return shortUrlArray.join("");
+};
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -15,8 +27,13 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase}; // When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable.
+  const templateVars = { urls: urlDatabase }; // When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable.
   res.render('urls_index', templateVars);
 });
 
@@ -27,7 +44,7 @@ app.get('/urls/new', (req, res) => {
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
-  });
+});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
