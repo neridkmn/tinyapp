412680234b7e1be05236032b34fc9aae671f0067
Neriman Dikmen <sanerneriman@gmail.com>
2023-06-28 14:28:47 -0400

    Fix for loop

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ function generateRandomString() {
   let shortUrlArray = [];
   let alphanumericCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  for (i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     let randomIndex = Math.floor(Math.random() * alphanumericCharacters.length);
     shortUrlArray.push(alphanumericCharacters[randomIndex]);
   }
