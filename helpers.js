9585a57909d6b585a8b6fd3a537e8d7a930a7d64
Neriman Dikmen <sanerneriman@gmail.com>
2023-07-02 08:41:46 -0400

    Move generateRandomString and urlsForUser functions and export them

--- helpers.js
+++ helpers.js
@@ -8,6 +8,31 @@ function findUserByEmail(email, users) {
   return undefined;
 };
 
+function generateRandomString() {
+  let shortUrlArray = [];
+  let alphanumericCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  for (let i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * alphanumericCharacters.length);
+    shortUrlArray.push(alphanumericCharacters[randomIndex]);
+  }
+
+  return shortUrlArray.join("");
+};
+
+function urlsForUser(id, urlDatabase) { // helper func for loop over a database and return urls whose user id matches with the given user id.
+  const urls = {};
+  for (const item in urlDatabase) {
+    if (urlDatabase[item].userID === id) {
+      urls[item] = urlDatabase[item];
+    }
+  }
+  return urls;
+};
+
+
 module.exports = {
-  findUserByEmail
+  findUserByEmail,
+  generateRandomString,
+  urlsForUser
 };
